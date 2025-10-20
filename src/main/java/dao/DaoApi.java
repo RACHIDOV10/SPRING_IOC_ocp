246a572999d65aee98f5a43794cfe6d330c8aeba
RACHIDOV10 <97804718+RACHIDOV10@users.noreply.github.com>
2025-10-20 10:24:06 +0100

    first commit

---
+++ src/main/java/dao/DaoApi.java
@@ -0,0 +1,11 @@
+package dao;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+@Component("daoApi")
+public class DaoApi implements IDao {
+    @Override
+    public double getValue(){
+        return 220.0;
+    }
+}
